e1b9dec3a550aa03e3a9f49e7294d458b7e01351
Salma Amr <salmaamr1520@gmail.com.>
2026-09-21 11:13:05 +0300

    Add project files.

---
+++ Assignment-04/DeliveryAddress.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_04
+{
+    internal class DeliveryAddress
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+    }
+}
